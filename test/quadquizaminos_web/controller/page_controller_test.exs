bc893546895bae1bb8a936a4f7f073b6c8405f38
TraceyOnim <traceyonim22@gmail.com>
2021-02-23 14:56:30 +0300

    added integration test

---
+++ test/quadquizaminos_web/controller/page_controller_test.exs
@@ -0,0 +1,53 @@
+defmodule QuadquizaminosWeb.PageControllerTest do
+  use QuadquizaminosWeb.ConnCase
+
+  import Phoenix.LiveViewTest
+
+  test "user is restricted to access tetris game if not logged in", %{conn: conn} do
+    {:error, {:redirect, %{to: "/"}}} = live(conn, "/tetris")
+  end
+
+  test "non configured players are restricted from accessing tetris game", %{conn: conn} do
+    auth = %Ueberauth.Auth{
+      provider: :github,
+      info: %{
+        first_name: "John",
+        last_name: "Doe",
+        email: "john.doe@example.com",
+        image: "https://example.com/image.jpg"
+      }
+    }
+
+    conn =
+      conn
+      |> bypass_through(QuadquizaminosWeb.Router, [:browser])
+      |> get("/auth/github/callback")
+      |> assign(:ueberauth_auth, auth)
+      |> QuadquizaminosWeb.AuthController.callback(%{})
+
+    assert get_flash(conn, :error) == "You are not authorized to access the page"
+  end
+
+  test "configured players get notified if Successfully authenticated", %{conn: conn} do
+    auth = %Ueberauth.Auth{
+      provider: :github,
+      info: %{
+        first_name: "John",
+        last_name: "Doe",
+        email: "john.doe@example.com",
+        image: "https://example.com/image.jpg",
+        name: "John Doe"
+      },
+      uid: 4_000_000
+    }
+
+    conn =
+      conn
+      |> bypass_through(QuadquizaminosWeb.Router, [:browser])
+      |> get("/auth/github/callback")
+      |> assign(:ueberauth_auth, auth)
+      |> QuadquizaminosWeb.AuthController.callback(%{})
+
+    assert get_flash(conn, :info) == "Successfully authenticated."
+  end
+end
