5d1f8928a91680c40921c345129308af3821f833
Duncan Sparrell <duncan@sfractal.com>
2021-03-02 07:54:48 -0500

    fix missing space

--- config/config.exs
+++ config/config.exs
@@ -43,12 +43,12 @@ config :quadquizaminos,
   # topics of QnA
   topics: [:supply_chain, :sbom, :open_c2, :open_chain, :phoenix, :sponsors],
   # name and questions in :supply_chain topic
-  supply_chain: %{name: "Supply Chain", question_number: 1, total_questions: 3, questions:["001","002","003"] },
-  sbom: %{name: "SBOM", question_number: 1, total_questions: 4, questions:["001","002","003", "004"] },
-  open_c2: %{name: "OpenC2", question_number: 1, total_questions: 1, questions:["001"] },
-  open_chain: %{name: "Open Chain", question_number: 1, total_questions: 1, questions:["001"] },
-  phoenix: %{name: "Phoenix", question_number: 1, total_questions: 1, questions:["001"] },
-  sponsors: %{name: "Sponsors", question_number: 1, total_questions: 1, questions:["001"] }
+  supply_chain: %{name: "Supply Chain", question_number: 1, total_questions: 3, questions: ["001","002","003"] },
+  sbom: %{name: "SBOM", question_number: 1, total_questions: 4, questions: ["001","002","003", "004"] },
+  open_c2: %{name: "OpenC2", question_number: 1, total_questions: 1, questions: ["001"] },
+  open_chain: %{name: "Open Chain", question_number: 1, total_questions: 1, questions: ["001"] },
+  phoenix: %{name: "Phoenix", question_number: 1, total_questions: 1, questions: ["001"] },
+  sponsors: %{name: "Sponsors", question_number: 1, total_questions: 1, questions: ["001"] }
 
 
 # Import environment specific config. This must remain at the bottom
