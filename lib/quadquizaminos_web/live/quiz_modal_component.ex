cff278c26b0d6bcde26acc8c093a9abbcdeec623
TraceyOnim <traceyonim22@gmail.com>
2021-03-03 10:43:26 +0300

    display qna categories

--- lib/quadquizaminos_web/live/quiz_modal_component.ex
+++ lib/quadquizaminos_web/live/quiz_modal_component.ex
@@ -4,22 +4,19 @@ defmodule QuadquizaminosWeb.QuizModalComponent do
   def render(assigns) do
     ~L"""
     <div>
-     <h2><%= raw @qna.question %></h2>
-     <h2> Answer </h2>
-    <%= f =  form_for :quiz, "#", phx_submit: :check_answer %>
-    <%= for {answer, index}<- @qna.answers do %>
-    <%= label do %>
-      <%= radio_button f, :guess, answer, value: index %>
-      <%= answer %>
-     <% end %> <!-- end label -->
+
+    <button phx-click="unpause">Continue</button>
+    <%= for category <- categories() do %>
+     <button phx-click="choose_category" phx-value-category="<%= category%>"><%= category %></button>
     <% end %>
-
-    <%= submit  "Continue" %>
-
-
-    </form>
-    <button phx-click="unpause">Power Up</button>
     </div>
     """
+  end
+
+  defp categories do
+    "qna"
+    |> File.ls!()
+    |> Enum.filter(fn folder -> File.dir?("./qna/#{folder}") end)
+    |> Enum.map(fn folder -> Macro.camelize(folder) end)
   end
 end
