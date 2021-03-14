c562d0c14eb82411dc18a9cc4dd51e93bda702f4
TraceyOnim <traceyonim22@gmail.com>
2021-03-14 16:24:32 +0300

    separate answers from powerups

--- lib/quadquizaminos/qna.ex
+++ lib/quadquizaminos/qna.ex
@@ -36,7 +36,7 @@ defmodule Quadquizaminos.QnA do
 
     %{
       question: question_as_html(question),
-      answers: answers(answers),
+      answers: answers(content, answers),
       correct: correct_answer(answers),
       powerup: powerup(content)
     }
@@ -51,6 +51,22 @@ defmodule Quadquizaminos.QnA do
 
       nil ->
         nil
+    end
+  end
+
+  defp answers(content, answers) do
+    regex = ~r/# answers(?<answers>.*)#/iU
+
+    case Regex.named_captures(regex, content |> String.replace("\n", " ")) do
+      %{"answers" => answers} ->
+        answers
+        |> String.trim()
+        |> String.split(["-", "*"], trim: true)
+        |> Enum.with_index()
+
+      nil ->
+        nil
+        answers(answers)
     end
   end
 
