47004ad314d39dcd05bc01b3a9e70c30088881a1
StickyFloors <73035577+StickyFloors@users.noreply.github.com>
2021-03-28 14:24:41 -0400

    Update instructions.ex
    Instruction copy changes pulled against v0.5-rc per previous PR's response.

--- lib/quadquizaminos_web/live/instructions.ex
+++ lib/quadquizaminos_web/live/instructions.ex
@@ -3,6 +3,41 @@ defmodule QuadquizaminosWeb.Instructions do
 
     def game_instruction() do
     """
+    <h2>Explore Cyber Security's Most Important Emerging Strategic Focus with the Digital Era's Most Beloved Game</h2>
+    <p>We proudly present the SupplyChainSandbox edition of QuadBlocksQuiz -
+       a reimagined take on Tetris where playful spatial negotiations are infused with 
+       real-life Trivia challenges from the world of supply chain security.</p>
+    <h2>Why Play?</h2>
+    <p>Whether you're attending RSAC because you "sell security" or "buy security," you can't afford 
+       to pass on this love child of Tetris and Trivia. More than abstract configurations of digital blocks, 
+       our Tetris spin-off envisions the classic arcade game as a potent metaphor for supply chain cyber security. 
+       You can choose to let the blocks fall as they may and scramble to cope with the accumulated consequences at 
+       a later point in time, or you can utilize savvy forecasting and informational superiority to address each 
+       element as it appears on the horizon.</p>
+     <p>As recent events have shown, you are only as protected as the weakest link in your supply chain. 
+       Guarding your organization’s information in a digitally-connected world
+       means understanding not only your organization’s immediate supply chain,
+       but also the security of the extended ecosystem and all entities involved. 
+       Besides the obvious enterprise advantage of robust supply chain security knowledge, 
+       the tidbits offered up here will reappear as in-game trivia challenges. </p>
+     <h2>Power Up!</h2>
+       <p>You don't need to be a supply chain expert - this game assumes you are a novice
+       and it teaches you what you need to know. You gain points by clearing rows as in a typical Tetris game. 
+       Here's the twist - you can answer quiz questions about supply chain both to gain points and to gain powerups.</p>
+      <p>Powerups help with classic Tetris strategy but also are needed
+       to combat:</p>
+        <ul>
+        <li>vulnerabilities (unfortunate gaps in an otherwise functioning security paradigm)</li> 
+        <li>licensing issues (extraneous blocks that gunk up the works)</li> 
+        <li>cyber attacks(rapid changes in operating conditions that take over entire sections
+            of the gameboard and speed the game up uncontrollably)</li>
+        <li>licensing lawsuits (tiresome procedures that gum up entire sections of the game board
+            and slow the game to a snail's pace)</li>
+        </ul>
+       <p>Just like in real life, information powerups can prevent attacks 
+            and address emerging vulnerabilities as the operational environment speeds up and slows down around us. 
+            Play the game and find out!
+        </p>
     <h2>How to play</h2>
         <ol>
           <li>Up arrow key rotates the blocks</li>
@@ -13,6 +48,7 @@ defmodule QuadquizaminosWeb.Instructions do
           <li>Debug until powerups: "l" lowers dropping speed</li>
           <li>Debug until powerups: "c" clears bottom blocks</li>
         </ol>
+        
     """
     end
 
