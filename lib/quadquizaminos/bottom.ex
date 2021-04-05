418a49c0250f6914a6339ddd0e791cfd32f824a9
sparrell <duncan@sfractal.com>
2021-04-05 12:46:25 -0400

    yab

--- lib/quadquizaminos/bottom.ex
+++ lib/quadquizaminos/bottom.ex
@@ -84,24 +84,23 @@ defmodule Quadquizaminos.Bottom do
        end)
   end
 
-  def add_vulnerability(bottom) do
-    ## pick random bottom block and change to add_vulnerability
-    ## two cases - empty map or map with blocks
-    case bottom do
-      %{} ->
-        %{{1, 20} => {1, 20, :vuln_grey_yellow}}
-      _ ->
-        {{x, y}, _} = Enum.random(bottom)
-        bottom
-        |> Map.merge(
-          %{{x, y} => {x, y, :vuln_grey_yellow}},
-            fn _k, _prev_value, vuln_value ->
-              vuln_value
-            end)
-    end
+  def add_vulnerability(bottom) when bottom == %{} do
+    ## empty input map, so stick one in anyway
+    %{{1, 20} => {1, 20, :vuln_grey_yellow}}
   end
 
-  def add_license_issue(%{} = _bottom) do
+  def add_vulnerability(bottom) do
+    ## pick random bottom block and change to add_vulnerability
+    {{x, y}, _} = Enum.random(bottom)
+    bottom
+    |> Map.merge(
+      %{{x, y} => {x, y, :vuln_grey_yellow}},
+        fn _k, _prev_value, vuln_value ->
+          vuln_value
+        end)
+  end
+
+  def add_license_issue when bottom == %{} do
     ## if no blocks to have issue, add one anyway
     %{{1, 20} => {1, 20, :license_grey_brown}}
   end
