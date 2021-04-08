1873007b4326f0f5edebafdcba1c76d0e6020799
Tracey Onim <43263401+TraceyOnim@users.noreply.github.com>
2021-04-08 11:02:42 +0300

    Fix bug in answers that resulted in wrong answer being returned (#202)
    * search correct answers with asterisk in new line
    
    * add test

--- test/quadquizaminos/qna_test.exs
+++ test/quadquizaminos/qna_test.exs
@@ -11,4 +11,18 @@ defmodule Quadquizaminos.QnaTest do
 
     assert Enum.count(answers) == 2
   end
+
+  test "question/1 returns the correct answers" do
+    _answers = [
+      {" short for fuddy-duddy", 0},
+      {" random guessed*answer", 1},
+      {" Fear, Uncertainty, Doubt", 2},
+      {" Fair Under Duress", 3},
+      {" an impolite term not suitable to repeat here", 4}
+    ]
+
+    actual_answer = "2"
+    %{correct: expected_answer} = QnA.question("risk")
+    assert actual_answer == expected_answer
+  end
 end
