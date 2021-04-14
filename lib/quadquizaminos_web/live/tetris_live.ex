0c41a17d7650d6a44f3d306a7f9a2f16c856091c
WanjikuMac <edwardscaroline32@gmail.com>
2021-04-14 10:16:26 +0300

    fix vuln and license power up for single block

--- lib/quadquizaminos_web/live/tetris_live.ex
+++ lib/quadquizaminos_web/live/tetris_live.ex
@@ -572,7 +572,15 @@ defmodule QuadquizaminosWeb.TetrisLive do
 
   def handle_event("powerup", %{"powerup" => "fixvuln"}, socket) do
     powers = socket.assigns.powers -- [:fixvuln]
-    {:noreply, assign(socket, powers: powers)}
+    {:noreply, socket |> assign(modal: false, powers: powers, fix_vulnerability: true)}
+  end
+
+  def handle_event("move_or_delete_block", %{"x" => x, "y" => y, "color" => color},  %{assigns: %{fix_vulnerability: true}} = socket) do
+    IO.inspect({x, y, color})  
+    {x, y} = parse_to_integer(x,y)
+    color = String.to_atom(color)  
+    bottom = Bottom.remove_single_vuln(socket.assigns.bottom, {x, y, color})   
+    {:noreply, socket |> assign(bottom: bottom)} 
   end
 
   def handle_event("powerup", %{"powerup" => "fixlicense"}, socket) do
