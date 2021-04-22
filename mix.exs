33c4974a09cbd9fada015bcbaaf1b0b80dc6b8e3
Duncan Sparrell <duncan@sfractal.com>
2021-04-21 20:51:21 -0400

    inc version for main

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Quadquizaminos.MixProject do
   def project do
     [
       app: :quadquizaminos,
-      version: "0.5.7",
+      version: "0.5.8",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
