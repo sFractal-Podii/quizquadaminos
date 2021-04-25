c2a78e9689423bcfc0e423912d2b84519ac0b8fd
WanjikuMac <edwardscaroline32@gmail.com>
2021-04-25 18:21:05 +0300

    add facebook configuration

--- config/config.exs
+++ config/config.exs
@@ -28,6 +28,15 @@ config :phoenix, :json_library, Jason
 
 config :ueberauth, Ueberauth,
   providers: [
+    facebook: {Ueberauth.Strategy.Facebook, []}
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Facebook.OAuth,
+      client_id: System.get_env("FACEBOOK_CLIENT_ID"),
+      client_secret: System.get_env("FACEBOOK_CLIENT_SECRET")  
+      
+config :ueberauth, Ueberauth,
+  providers: [
     github: {Ueberauth.Strategy.Github, [default_scope: "user:email", allow_private_emails: true]}
   ]
 
