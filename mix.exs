34f7618da290bd45d59278e3d819d7d75ffbb2e6
WanjikuMac <edwardscaroline32@gmail.com>
2021-04-25 18:28:16 +0300

    add missing comma

--- mix.exs
+++ mix.exs
@@ -61,7 +61,7 @@ defmodule Quadquizaminos.MixProject do
       {:plug_cowboy, "~> 2.0"},
       {:ueberauth, "~> 0.6"},
       {:ueberauth_github, "~> 0.7"},
-      {:ueberauth_facebook, "~> 0.8"}
+      {:ueberauth_facebook, "~> 0.8"},
       {:sbom, git: "https://github.com/voltone/sbom", runtime: false},
       {:earmark, "~> 1.4"}
     ]
