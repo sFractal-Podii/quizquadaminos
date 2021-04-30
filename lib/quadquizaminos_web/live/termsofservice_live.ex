a62a616355fd9333aa50b585fe77a568cdfb5924
WanjikuMac <edwardscaroline32@gmail.com>
2021-04-30 09:51:19 +0300

    add terms of service and privacy page

---
+++ lib/quadquizaminos_web/live/termsofservice_live.ex
@@ -0,0 +1,9 @@
+defmodule QuadquizaminosWeb.TermsOfServiceLive do
+    use Phoenix.LiveView
+
+    def render(assigns) do
+        ~L"""
+        <h1>Terms Of Service</h1>
+        """
+      end
+end
