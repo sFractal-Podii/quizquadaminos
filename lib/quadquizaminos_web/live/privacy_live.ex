40803ba66d2867734f3c050fb0234a8df6cf738a
sparrell <duncan@sfractal.com>
2021-05-03 16:59:39 -0400

    update privacy policy

--- lib/quadquizaminos_web/live/privacy_live.ex
+++ lib/quadquizaminos_web/live/privacy_live.ex
@@ -4,6 +4,84 @@ defmodule QuadquizaminosWeb.PrivacyLive do
     def render(assigns) do
         ~L"""
         <h1>Privacy Policy</h1>
+        <h3>Version 1.0.0 (last updated 3-May-2021)</h3>
+
+        sFractal knows you take you privacy seriously
+        and we do too.
+        We attempt to collect as little information as possible,
+        be transparent,
+        keep the information no longer than necessary,
+        and give you control over what information we collect about you.
+
+        <h2>What data is collected by QuadBlockQuiz?</h2>
+        We believe we are only collecting publicly accessbile information.
+        If you login using our anonymous feature,
+        we are collecting/keeping no information at all.
+        If you login using OAUTH from GitHub, Google, Twitter, LinkedIn
+        then we are only keeping enough information to display your score,
+        and to contact you shoudl you win a prize.
+
+        <h2>How is my data used by QuadBlockQuiz?</h2>
+        We use your data to operate, maintain, and improve our services.
+        In particular, your identiy (if provided by OAUTH from GitHub,
+        Google, Twitter, or LinkedIn) is used to display your score
+        on the Leaderboard and on the Context Board.
+        We may use your contact information from OAUTH to inform you of prizes
+        should you win our contest.
+
+        <h2>How is my data shared?</h2>
+        Your data
+        (which we believe to only be publicly accessible information)
+        will not be shared other than
+        (1) if we are required to do so by law
+        (2) to establish, exercise, or defend our legal rights
+        (3) when we believe disclosure is necessary or appropriate to prevent
+        physical, fiancial, or other harm
+        (4) in conjunction with an investigation of suspected or actual illegal activity
+        (5) to protect the rights, property, or safety of sFractal or it's users.
+
+        <h2>What about cookies?</h2>
+        We may use techologies such as cookies, local storage,
+        web sessions, websockets
+        and other automated tecnologies on our websites.
+        We use these technologies for a number of reasons
+        including authentication, authorization, security,
+        and diagnostics.
+
+        You can control cookies through your browswer settings
+        and other tools.
+        Note that rejecting cookies may affect your ability
+        to interact with the website.
+
+        <h2>How do I access or delete my data?</h2>
+        Contact quadblockquiz@googlegroups.com
+
+        <h2>Retention</h2>
+        To the extent possible, we keep no data longer than necessary;
+        and will delete all data within one year unless we have
+        the owner's consent to retain for a specific purpose
+        (e.g. bragging rights to remain on Leaderboard).
+
+        <h2>How do we protect your information?</h2>
+        We intend to only collect publicly available information
+        and we maintain administrative, technical, and physical
+        safeguards to protect any data we maintain against
+        accidental, unlawful, or unauthorized
+        destruction, loss, adulteration, access, distortion, or use.
+
+        <h2>Children under 13</h2>
+        This website is intended for a general audience
+        and is not directed at children.
+        We do not knowlingly collect personal information
+        online from indivuals under the age of 13
+        or such other age as may be directed by applicable law.
+
+        <h2>Changes to this policy</h2>
+        sFractal may amend this policy from time to time.
+        We encourage you to regularly check this page to review any changes.
+        Near the top of this page, we will indicate when
+        it was most recently updated.
+
         """
       end
 end
