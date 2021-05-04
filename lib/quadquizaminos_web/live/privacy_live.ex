097b4678ac3ecb9a287d9cfe526be2eb5a2075d1
sparrell <duncan@sfractal.com>
2021-05-04 18:33:18 -0400

    finishing up privacy policy

--- lib/quadquizaminos_web/live/privacy_live.ex
+++ lib/quadquizaminos_web/live/privacy_live.ex
@@ -4,7 +4,7 @@ defmodule QuadquizaminosWeb.PrivacyLive do
     def render(assigns) do
         ~L"""
         <h1>Privacy Policy</h1>
-        <h3>Version 1.0.0 (last updated 3-May-2021)</h3>
+        <h3>Version 1.0.0 (last updated 4-May-2021)</h3>
 
         sFractal knows you take you privacy seriously
         and we do too.
@@ -12,6 +12,19 @@ defmodule QuadquizaminosWeb.PrivacyLive do
         be transparent,
         keep the information no longer than necessary,
         and give you control over what information we collect about you.
+
+        This Privacy Policy describes Our policies and procedures on the
+        collection, use and disclosure of Your information
+        when You use the Service and tells You about Your privacy rights
+        and how the law protects You.
+
+        We use Your Personal data to provide and improve the Service.
+        By using the Service,
+        You agree to the collection
+        and use of information in accordance with this Privacy Policy.
+        Parts of this Privacy Policy has been created
+        with the help of the Privacy Policy Generator.
+
 
         <h2>What data is collected by QuadBlockQuiz?</h2>
         We believe we are only collecting publicly accessbile information.
@@ -39,6 +52,22 @@ defmodule QuadquizaminosWeb.PrivacyLive do
         physical, fiancial, or other harm
         (4) in conjunction with an investigation of suspected or actual illegal activity
         (5) to protect the rights, property, or safety of sFractal or it's users.
+
+        Information from Third-Party Social Media Services
+        The Company allows You to create an account
+        and log in to use the Service
+        through the following Third-party Social Media Services:
+        GitHub,
+        Google,
+        Facebook,
+        Twitter,
+        LinkedIn.
+        If You decide to register through
+        or otherwise grant us access to a Third-Party Social Media Service,
+        We may collect Personal data that is already associated
+        with Your Third-Party Social Media Service's account,
+        such as Your name, or Your email address,
+        associated with that account.
 
         <h2>What about cookies?</h2>
         We may use techologies such as cookies, local storage,
@@ -76,11 +105,35 @@ defmodule QuadquizaminosWeb.PrivacyLive do
         online from indivuals under the age of 13
         or such other age as may be directed by applicable law.
 
+        <h2>Links to Other Websites</h2>
+        Our Service may contain links to other websites
+        that are not operated by Us.
+        If You click on a third party link,
+        You will be directed to that third party's site.
+        We strongly advise You to review the Privacy Policy
+        of every site You visit.
+
+        We have no control over and assume no responsibility
+        for the content, privacy policies or practices
+        of any third party sites or services.
+
         <h2>Changes to this policy</h2>
         sFractal may amend this policy from time to time.
         We encourage you to regularly check this page to review any changes.
         Near the top of this page, we will indicate when
         it was most recently updated.
+
+        <h2>Security of Your Personal Data</h2>
+        The security of Your Personal Data is important to Us,
+        but remember that no method of transmission over the Internet,
+        or method of electronic storage is 100% secure.
+        While We strive to use commercially acceptable means
+        to protect Your Personal Data,
+        We cannot guarantee its absolute security.
+
+        <h1> draft </h1>
+
+
 
         """
       end
