e3185325441d09a5189ff2aa2c2285a35a31b131
TraceyOnim <traceyonim22@gmail.com>
2021-05-05 11:47:14 +0300

    update Duncan's role to admin

---
+++ priv/repo/migrations/20210505084147_rectify_existing_user_role.exs
@@ -0,0 +1,17 @@
+defmodule Quadquizaminos.Repo.Migrations.RectifyExistingUserRole do
+  use Ecto.Migration
+
+  def up do
+    execute "
+   UPDATE users 
+   SET role = CASE
+              WHEN user_id IN (43263401, 1723279, 32665021, 584211)  THEN
+              'admin'
+              ELSE 'player'
+              END 
+   "
+  end
+
+  def down do
+  end
+end
