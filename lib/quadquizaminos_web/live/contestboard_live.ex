4811395c1a0b8789d2d4cc5c31e1e18ad2d6e2c1
WanjikuMac <edwardscaroline32@gmail.com>
2021-05-10 11:04:46 +0300

    use actual time

--- lib/quadquizaminos_web/live/contestboard_live.ex
+++ lib/quadquizaminos_web/live/contestboard_live.ex
@@ -6,9 +6,11 @@ defmodule QuadquizaminosWeb.ContestboardLive do
   @initial_second 59
   def mount(_params, _session, socket) do
     :timer.send_interval(1000, self(), :count_down)
-    {:ok, socket |> assign(hours: @initial_hour, minutes: @initial_minute, seconds: @initial_second , start_date: DateTime.utc_now())}
+    start_date = DateTime.utc_now()
+    time = current_time(start_date) 
+    {:ok, socket |> assign(hours: current_hour(time), minutes: current_minute(time), seconds: current_second(time), start_date: start_date)}
   end
-  
+
 #Next step, use current hour, minutes and seconds
 
   def render(assigns) do
@@ -22,17 +24,32 @@ defmodule QuadquizaminosWeb.ContestboardLive do
   end
 
   def handle_info(:count_down, socket) do
-    IO.inspect(socket.assigns.start_date)
-    
+    start_date = DateTime.utc_now |> IO.inspect
      seconds = second_count(socket.assigns.seconds) 
      new_minute = minute_count(socket, seconds)
      hours = hour_count(socket, socket.assigns.minutes)
-    {:noreply, socket |> assign(seconds: seconds , minutes: new_minute, hours: hours)}
+    {:noreply, socket |> assign(seconds: seconds , minutes: new_minute, hours: hours, start_date: start_date)}
   end
 
   def date_count(start_date) do
     d_day_time = Application.fetch_env!(:quadquizaminos, :contest_date)
     Date.diff(DateTime.to_date(d_day_time), DateTime.to_date(start_date))
+  end
+
+  def current_time(start_date) do
+    start_date |> DateTime.to_time 
+  end
+
+  defp current_hour(time) do
+     @initial_hour - time.hour
+  end
+
+  defp current_minute(time) do
+    @initial_minute - time.minute
+  end
+
+  defp current_second(time) do
+    @initial_second - time.second
   end
 
   def second_count(seconds) do
@@ -66,7 +83,7 @@ defmodule QuadquizaminosWeb.ContestboardLive do
   end
 
   def hour_count(socket, minutes)do
-    if minutes == 0 and socket.assigns.seconds == 0 do
+    if minutes == 59 and socket.assigns.seconds == 0 do
       hour_count(socket.assigns.hours)
     else
       socket.assigns.hours 
