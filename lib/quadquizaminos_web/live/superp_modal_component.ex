44e60d7fc84c0fc4cde97bedf72aaee5f8966d4b
sparrell <duncan@sfractal.com>
2021-05-16 16:07:25 -0400

    superpower implementation - first (of probably many) try

--- lib/quadquizaminos_web/live/superp_modal_component.ex
+++ lib/quadquizaminos_web/live/superp_modal_component.ex
@@ -5,70 +5,16 @@ defmodule QuadquizaminosWeb.SuperpModalComponent do
   def render(assigns) do
     ~L"""
     <div style="text-align:center;">
-
-    power = :deleteblock
-    icon  = "fa-minus-square"
-    descr = "remove a block"
-    <%= powers_button(power, icon, descr) %>
-
-    power = :addblock
-    icon  = "fa-plus-square"
-    descr = "add a block"
-    <%= powers_button(power, icon, descr) %>
-
-    :moveblock -> "fa-arrows-alt"
-    power = :x
-    icon  = "fa-x"
-    descr = "x"
-    <%= powers_button(power, icon, descr) %>
-
-    power = :x
-    icon  = "fa-x"
-    descr = "x"
-    <%= powers_button(power, icon, descr) %>
-
-    :clearblocks -> "fa-eraser"
-    power = :x
-    icon  = "fa-x"
-    descr = "x"
-    <%= powers_button(power, icon, descr) %>
-
-    :speedup -> "fa-fast-forward"
-    power = :x
-    icon  = "fa-x"
-    descr = "x"
-    <%= powers_button(power, icon, descr) %>
-
-    :slowdown -> "fa-fast-backward"
-    power = :x
-    icon  = "fa-x"
-    descr = "x"
-    <%= powers_button(power, icon, descr) %>
-
-    :fixvuln -> "fa-wrench"
-    power = :x
-    icon  = "fa-x"
-    descr = "x"
-    <%= powers_button(power, icon, descr) %>
-
-    :fixlicense -> "fa-screwdriver"
-    power = :x
-    icon  = "fa-x"
-    descr = "x"
-    <%= powers_button(power, icon, descr) %>
-
-    :rm_all_vulns -> "fa-hammer"
-    power = :x
-    icon  = "fa-x"
-    descr = "x"
-    <%= powers_button(power, icon, descr) %>
-
-    :rm_all_lic_issues -> "fa-tape"
-    power = :x
-    icon  = "fa-x"
-    descr = "x"
-    <%= powers_button(power, icon, descr) %>
-
+    <%= p_deleteblock() %>
+    <%= p_addblock() %>
+    <%= p_moveblock() %>
+    <%= p_clearblocks() %>
+    <%= p_speedup() %>
+    <%= p_slowdown() %>
+    <%= p_fixvuln() %>
+    <%= p_fixlicense() %>
+    <%= p_rm_all_vulns() %>
+    <%= p_rm_all_lic_issues() %>
     </div>
     """
   end
@@ -83,5 +29,74 @@ defmodule QuadquizaminosWeb.SuperpModalComponent do
     """
   end
 
+  defp p_deleteblock() do
+    power = :deleteblock
+    icon  = "fa-minus-square"
+    descr = "remove a block"
+    powers_button(power, icon, descr)
+  end
+
+  defp p_addblock() do
+    power = :addblock
+    icon  = "fa-plus-square"
+    descr = "add a block"
+    powers_button(power, icon, descr)
+  end
+
+  defp p_moveblock() do
+    power = :moveblock
+    icon  = "fa-arrows-alt"
+    descr = "move a block"
+    powers_button(power, icon, descr)
+  end
+
+  defp p_clearblocks() do
+    power = :clearblocks
+    icon  = "fa-eraser"
+    descr = "clear gameboard of all blocks"
+    powers_button(power, icon, descr)
+  end
+
+  defp p_speedup() do
+    power = :speedup
+    icon  = "fa-fast-forward"
+    descr = "make blocks fall faster"
+    powers_button(power, icon, descr)
+  end
+
+  defp p_slowdown() do
+    power = :slowdown
+    icon  = "fa-fast-backward"
+    descr = "make blocks fall slower"
+    powers_button(power, icon, descr)
+  end
+
+  defp p_fixvuln() do
+    power = :fixvuln
+    icon  = "fa-wrench"
+    descr = "fix one vulnerability"
+    powers_button(power, icon, descr)
+  end
+
+  defp p_fixlicense() do
+    power = :fixlicense
+    icon  = "fa-screwdriver"
+    descr = "fix one licensing issue"
+    powers_button(power, icon, descr)
+  end
+
+  defp p_rm_all_vulns() do
+    power = :rm_all_vulns
+    icon  = "fa-hammer"
+    descr = "remove all vulnerabilities"
+    powers_button(power, icon, descr)
+  end
+
+  defp p_rm_all_lic_issues() do
+    power = :rm_all_lic_issues
+    icon  = "fa-tape"
+    descr = "remove all licensing issues"
+    powers_button(power, icon, descr)
+  end
 
 end
