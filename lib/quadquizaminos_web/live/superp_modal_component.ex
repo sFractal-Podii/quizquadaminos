eb9c50420de9753ba1db08efe4b28f63d2f4870b
sparrell <duncan@sfractal.com>
2021-05-16 21:23:34 -0400

    rm icons from superpowers

--- lib/quadquizaminos_web/live/superp_modal_component.ex
+++ lib/quadquizaminos_web/live/superp_modal_component.ex
@@ -26,37 +26,31 @@ defmodule QuadquizaminosWeb.SuperpModalComponent do
 
     <br>
     <button phx-click="super_to_power" phx-value-spower="speedup">
-    <i class="fas fa-fast-forward" </i>
     make blocks fall faster
     </button>
 
     <br>
     <button phx-click="super_to_power" phx-value-spower="slowdown">
-    <i class="fas fa-fast-backward" </i>
     make blocks fall slower
     </button>
 
     <br>
     <button phx-click="super_to_power" phx-value-spower="fixvuln">
-    <i class="fas fa-wrench" </i>
     fix one vulnerability
     </button>
 
     <br>
     <button phx-click="super_to_power" phx-value-spower="fixlicense">
-    <i class="fas fa-screwdriver" </i>
     fix one licensing issue
     </button>
 
     <br>
     <button phx-click="super_to_power" phx-value-spower="rm_all_vulns">
-    <i class="fas fa-hammer" </i>
     remove all vulnerabilities
     </button>
 
     <br>
     <button phx-click="super_to_power" phx-value-spower="rm_all_lic_issues">
-    <i class="fas fa-tape" </i>
     remove all licensing issues
     </button>
 
