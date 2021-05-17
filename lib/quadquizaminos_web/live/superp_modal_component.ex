152f7558a206ee7329024fe14793781ac1f94a46
sparrell <duncan@sfractal.com>
2021-05-16 21:08:19 -0400

    format icons

--- lib/quadquizaminos_web/live/superp_modal_component.ex
+++ lib/quadquizaminos_web/live/superp_modal_component.ex
@@ -4,62 +4,62 @@ defmodule QuadquizaminosWeb.SuperpModalComponent do
   def render(assigns) do
     ~L"""
     <div style="text-align:center;">
-    <br><i class="fa-minus-square" </i>
+    <br><i class="fas fa-minus-square" </i>
     <button phx-click="super_to_power" phx-value-spower="deleteblock">
     remove a block
     </button>
 
     <br>
     <button phx-click="super_to_power" phx-value-spower="addblock">
-    <i class="fa-plus-square" </i>
+    <i class="fas fa-plus-square" </i>
     add a block
     </button>
 
     <br>
     <button phx-click="super_to_power" phx-value-spower="moveblock">
-    <i class="fa-arrows-alt" </i>
+    <i class="fas fa-arrows-alt" </i>
     move a block
     </button>
 
     <br>
     <button phx-click="super_to_power" phx-value-spower="clearblocks">
-    <i class="fa-eraser" </i>
+    <i class="fas fa-eraser" </i>
     clear gameboard of all blocks
     </button>
 
     <br>
     <button phx-click="super_to_power" phx-value-spower="speedup">
-    <i class="fa-fast-forward" </i>
+    <i class="fas fa-fast-forward" </i>
     make blocks fall faster
     </button>
 
     <br>
     <button phx-click="super_to_power" phx-value-spower="slowdown">
-    <i class="fa-fast-backward" </i>
+    <i class="fas fa-fast-backward" </i>
     make blocks fall slower
     </button>
 
     <br>
     <button phx-click="super_to_power" phx-value-spower="fixvuln">
-    <i class="fa-wrench" </i>
+    <i class="fas fa-wrench" </i>
     fix one vulnerability
     </button>
 
     <br>
     <button phx-click="super_to_power" phx-value-spower="fixlicense">
-    <i class="fa-screwdriver" </i>
+    <i class="fas fa-screwdriver" </i>
     fix one licensing issue
     </button>
 
     <br>
     <button phx-click="super_to_power" phx-value-spower="rm_all_vulns">
-    <i class="fa-hammer" </i>
+    <i class="fas fa-hammer" </i>
     remove all vulnerabilities
     </button>
 
     <br>
     <button phx-click="super_to_power" phx-value-spower="rm_all_lic_issues">
-    <i class="fa-tape" </i>
+    <i class="fas fa-tape" </i>
     remove all licensing issues
     </button>
 
