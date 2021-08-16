426d579d54d8d62092f06fb736177527937fda2d
emmyachieng <63039109+emmyachieng@users.noreply.github.com>
2021-08-16 20:02:19 +0300

    Admin dropdown in navbar  (#464)
    * added dropdown list on the header navbar and styling
    
    * added an icon for the admin dropdown

--- lib/quadquizaminos_web/views/layout_view.ex
+++ lib/quadquizaminos_web/views/layout_view.ex
@@ -1,3 +1,20 @@
 defmodule QuadquizaminosWeb.LayoutView do
   use QuadquizaminosWeb, :view
+
+  defp admin_dropdown do
+    ~E"""
+    <div class="dropdown">
+    <a href="/admin" class="dropbtn">
+    Admin
+    <i class="fa fa-caret-down"></i>
+    </a>
+     <div class="dropdown-content">
+     <a class="button" href="<%= Routes.contests_path(QuadquizaminosWeb.Endpoint, :index) %>">
+     Contest
+     </a>
+     <a class = "button" href="<%= Routes.live_path(QuadquizaminosWeb.Endpoint, QuadquizaminosWeb.AdminLive) %>">
+     Login
+     </a>
+    """
+  end
 end
