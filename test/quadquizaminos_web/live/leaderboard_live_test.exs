c723b77d0ec4ccb2532f74fc934dc305428cecee
WanjikuMac <edwardscaroline32@gmail.com>
2021-08-20 09:01:27 +0300

    add tests to ensure sort order is maintained and when sort varible is changed we go back to page one

--- test/quadquizaminos_web/live/leaderboard_live_test.exs
+++ test/quadquizaminos_web/live/leaderboard_live_test.exs
@@ -13,6 +13,8 @@ defmodule QuadquizaminosWeb.LeaderboardLiveTest do
     assert html =~ "<h1>Leaderboard</h1>"
   end
 
+  ## This test should propabably go since we are already testing rendering of 
+  ## 25 records at a time which is what the current implementation does 
   test "top 10 games played are displayed", %{conn: conn} do
     Enum.each(1..15, fn num ->
       attrs = %{name: "Quiz Block ", uid: Integer.to_string(num), role: "player"}
@@ -31,7 +33,7 @@ defmodule QuadquizaminosWeb.LeaderboardLiveTest do
     end)
 
     {:ok, _view, html} = live(conn, "/leaderboard")
-    assert row_count(html) == 10
+    assert row_count(html) == 15
     assert html =~ "<th>Player</th>"
     assert html =~ "<th>Start time</th>"
     assert html =~ "<th>End time</th>"
@@ -83,7 +85,7 @@ defmodule QuadquizaminosWeb.LeaderboardLiveTest do
           end_time: DateTime.utc_now(),
           uid: user.uid,
           score: num,
-          dropped_bricks: 10,
+          dropped_bricks: num,
           correctly_answered_qna: 2,
           bottom_blocks: bottom
         }
@@ -110,11 +112,28 @@ defmodule QuadquizaminosWeb.LeaderboardLiveTest do
       {:ok, view, html} = live(conn, "/leaderboard")
       assert html =~ "<td class=\"score\">97</td>"
 
-      html = view |> element("a[href='/leaderboard?page=2']") |> render_click()
+      html = view |> element("a[href='/leaderboard?page=2&sort_by=score']") |> render_click()
       refute html =~ "<td class=\"score\">97</td>"
     end
 
-    test "maintains sort order"
+    test "maintains sort order", %{conn: conn} do
+      {:ok, view, html} = live(conn, "/leaderboard")
+      assert html =~ "<td class=\"score\">97</td>"
+
+      html = view |> element("a[href='/leaderboard?page=2&sort_by=score']") |> render_click()
+      assert html =~ "<td class=\"score\">72</td>"
+      refute html =~ "<td class=\"score\">80</td>"
+    end
+
+    test "new sort order returns you to page 1", %{conn: conn} do
+      {:ok, view, html} = live(conn, "/leaderboard")
+      assert html =~ "<td class=\"score\">97</td>"
+
+      html = view |> element("a[href='/leaderboard?page=2&sort_by=score']") |> render_click()
+      assert html =~ "<td class=\"score\">72</td>"
+      html = render_click(view, "sort", %{param: "dropped_bricks"})
+      assert html =~ "<td class=\"score\">97</td><td>97</td>"
+    end
   end
 
   defp row_count(html) do
