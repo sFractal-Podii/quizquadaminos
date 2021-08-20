be29d61331fb3204be96e2d81fa21f89f92cb844
WanjikuMac <edwardscaroline32@gmail.com>
2021-08-20 10:23:27 +0300

    refine tests

--- test/quadquizaminos_web/live/leaderboard_live_test.exs
+++ test/quadquizaminos_web/live/leaderboard_live_test.exs
@@ -68,7 +68,9 @@ defmodule QuadquizaminosWeb.LeaderboardLiveTest do
 
   describe "pagination" do
     setup do
-      1..97
+      max = 97
+
+      1..max
       |> Enum.each(fn num ->
         bottom = %{
           <<1, 20>> => <<1, 20, 112, 105, 110, 107>>,
@@ -85,7 +87,7 @@ defmodule QuadquizaminosWeb.LeaderboardLiveTest do
           end_time: DateTime.utc_now(),
           uid: user.uid,
           score: num,
-          dropped_bricks: num,
+          dropped_bricks: max + 1 - num,
           correctly_answered_qna: 2,
           bottom_blocks: bottom
         }
@@ -118,21 +120,28 @@ defmodule QuadquizaminosWeb.LeaderboardLiveTest do
 
     test "maintains sort order", %{conn: conn} do
       {:ok, view, html} = live(conn, "/leaderboard")
-      assert html =~ "<td class=\"score\">97</td>"
+      assert html =~ "<td class=\"score\">97</td><td>1</td>"
 
-      html = view |> element("a[href='/leaderboard?page=2&sort_by=score']") |> render_click()
-      assert html =~ "<td class=\"score\">72</td>"
-      refute html =~ "<td class=\"score\">80</td>"
+      html = render_click(view, "sort", %{param: "dropped_bricks"})
+      assert html =~ "<td class=\"score\">1</td><td>97</td>"
+
+      html =
+        view |> element("a[href='/leaderboard?page=2&sort_by=dropped_bricks']") |> render_click()
+
+      assert html =~ "<td class=\"score\">26</td><td>72</td>"
+      refute html =~ "<td class=\"score\">1</td><td>97</td>"
     end
 
     test "new sort order returns you to page 1", %{conn: conn} do
       {:ok, view, html} = live(conn, "/leaderboard")
-      assert html =~ "<td class=\"score\">97</td>"
+      assert html =~ "<td class=\"score\">97</td><td>1</td>"
 
       html = view |> element("a[href='/leaderboard?page=2&sort_by=score']") |> render_click()
-      assert html =~ "<td class=\"score\">72</td><td>72</td>"
+      assert html =~ "<td class=\"score\">72</td><td>26</td>"
+      assert html =~ "href=\"/leaderboard?page=7&amp;sort_by=score\""
       html = render_click(view, "sort", %{param: "dropped_bricks"})
-      assert html =~ "<td class=\"score\">97</td><td>97</td>"
+      assert html =~ "<td class=\"score\">1</td><td>97</td>"
+      refute html =~ "href=\"/leaderboard?page=7&amp;sort_by=dropped_bricks\""
     end
   end
 
