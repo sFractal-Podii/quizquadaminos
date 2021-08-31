5d83d3db023184951d7e4182a7868b13cafdf9ac
TraceyOnim <traceyonim22@gmail.com>
2021-08-31 12:22:02 +0300

    add unit test

--- test/quadquizaminos/contests/contest/rsvp_test.exs
+++ test/quadquizaminos/contests/contest/rsvp_test.exs
@@ -5,11 +5,28 @@ defmodule Quadquizaminos.ContestsTest.RSVP do
   alias Quadquizaminos.Contests
   alias Quadquizaminos.Contests.RSVP
 
-  test "create_rsvp/2 creates a new rsvp on db" do
-    {:ok, current_user} = Accounts.create_user(%User{}, %{name: "ha", uid: "34"})
-    {:ok, contest} = Contests.create_contest(%{name: "test contest"})
-    attrs = %{"contest_id" => contest.id}
+  describe "RSVP" do
+    setup do
+      {:ok, current_user} = Accounts.create_user(%User{}, %{name: "ha", uid: "34"})
+      {:ok, contest} = Contests.create_contest(%{name: "test contest"})
+      attrs = %{"contest_id" => contest.id}
 
-    {:ok, %RSVP{}} = Contests.create_rsvp(attrs, current_user)
+      [attrs: attrs, current_user: current_user, contest: contest]
+    end
+
+    test "create_rsvp/2 creates a new rsvp on db", %{attrs: attrs, current_user: current_user} do
+      {:ok, %RSVP{}} = Contests.create_rsvp(attrs, current_user)
+    end
+
+    test "cancel_rsvp/2 deletes existing rsvp on db", %{
+      attrs: attrs,
+      current_user: current_user,
+      contest: contest
+    } do
+      {:ok, %RSVP{}} = Contests.create_rsvp(attrs, current_user)
+      {1, [%RSVP{contest_id: id}]} = Contests.cancel_rsvp(contest.id, current_user)
+
+      refute Repo.get_by(RSVP, contest_id: id)
+    end
   end
 end
