4cb96691b3df6932837cf95d20854763a7e6ef23
Sigu Magwa <sigumagwa@gmail.com>
2021-08-31 15:11:46 +0300

    define question struct

---
+++ lib/quadquizaminos/questions/question.ex
@@ -0,0 +1,19 @@
+defmodule Quadquizaminos.Questions.Question do
+  # @enforce_keys [:type, :question]
+  @type t :: %__MODULE__{
+          type: atom(),
+          question: String.t(),
+          title: String.t(),
+          score: map(),
+          description: String.t(),
+          powerup: atom(),
+          choices: list()
+        }
+  defstruct title: nil,
+            type: nil,
+            question: nil,
+            description: nil,
+            choices: [],
+            score: %{right: 0, wrong: 0},
+            powerup: nil
+end
