0bad365763cea05993e5629d2448488e03268414
Tracey Onim <43263401+TraceyOnim@users.noreply.github.com>
2021-09-15 14:38:40 +0300

    Login with handle (#520)
    * Enable user to login with handle
    
    * transfer login anonmously implementation
    
    * use existing user as current_user if they already exist
    
    * put it as first option
    
    * use user uid
    
    Co-authored-by: Duncan Sparrell <sparrell@users.noreply.github.com>

--- lib/quadquizaminos_web/router.ex
+++ lib/quadquizaminos_web/router.ex
@@ -48,8 +48,9 @@ defmodule QuadquizaminosWeb.Router do
     live "/courses", CourseLive
     live "/courses/:course", CourseLive, :show
     live "/courses/:course/:chapter", CourseLive, :questions
-    get "/anonymous", PageController, :anonymous
-    post "/anonymous", PageController, :anonymous
+    get "/anonymous", SessionController, :anonymous
+    post "/anonymous", SessionController, :anonymous
+    resources "/sessions", SessionController, only: [:new, :create]
     get "/instructions", PageController, :instructions
 
     pipe_through :authorize
