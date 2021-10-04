62e95b2156e30d34f791ca8aa8ed9d232cd4c9c0
sparrell <duncan@sfractal.com>
2021-10-03 20:30:28 -0400

    update contest rules and prizes

--- lib/quadquizaminos_web/live/contest_rules.ex
+++ lib/quadquizaminos_web/live/contest_rules.ex
@@ -6,12 +6,15 @@ defmodule QuadquizaminosWeb.ContestRules do
     ~L"""
     <h1>Contest Rules</h1>
     <p>
-    There will two contests: "1-hour" and "24-hour".
-    These contests are organized and sponsored by sFractal Consulting
+    These are the rules for the
+    AT&T Software Symposium Security Workshop
+    QuadBlockQuiz contest.
+    Prizes are described at
+    <a href= <%= Routes.live_path(QuadquizaminosWeb.Endpoint, QuadquizaminosWeb.ContestPrizes) %> >prizes</a>.
+    This contests was organized by sFractal Consulting
     to increase awarenessa and adoption of supply chain cybersecurity.
     </p>
     <p>
-    Both of these contests are governed by these official rules.
     By participating in a contest,
     each entrant agrees to abide by these rules,
     including all eligibility requirements,
@@ -22,117 +25,76 @@ defmodule QuadquizaminosWeb.ContestRules do
     and regulations and is void where prohibited by law.
     </p>
     <p>
-    Contestants must be BSidesLV attendess who are at least 21 years of age or older to win prizes.
-    Contestants must be on
-    <a href="https://discord.com/invite/YcCWqMW5">
-    BSidesLV Discord
-    </a>
+    Contestants must be
+    AT&T employees attending the Cybersecurity Workshop
+    at the AT&T Software Symposium.
+    Contestants must be least 21 years of age or older to win prizes.
+    </p>
+    <p>
+    Contestants must logon to the game using the 'handle' option
+    (NOT github, google, anonymous, etc).
+    Contestants may use any handle they want,
+    but recognize there are over 500 participants so there is the change of overlap.
+    The game does not check for overlap so pick something unique.
+    Your ATTUID is unique but you probably want to combine it with something
+    e.g. ds_sFractal_6284.
+    You can play with more than one handle - but one person can only win one prize!
+    The game should prompt you for your email when you first start to play.
+    Once assigned, that handle will stay connected to that email address.
+    If it didn't prompt you for your email, you might have chosen and existing handle.
+    </p>
+    <p>
+    Only valid AT&T emails are elibigle to win prizes.
+    The organizers must be able to
     to communicate with contest organizer about reciving their prize.
+    More importantly, the ATTUID is used to recieve MyRewards.
     </p>
     <p>
-    For the "1-hour" contest,
-    contestants must be participating live in the
-    <a href="https://www.bsideslv.org/talks#1045530">
-    "QuadBlockQuiz - Supply Chain Sandbox Edition"
-    </a>
-    session to be elible to win.
-    Prizes for the "1-hour" contest will be announced during the Q&A session after the talk.
-    </p>
-    <p>
-    Contestants must be logged in to QuadBlockQuiz with an identifiable id (i.e. not 'anonymous') to be able to win.
-    </p>
-    <p>
-    To be eligible for prizes, a game must start after the contest start time
-    and must end cleanly before the contest end time.
-    Both contests will start
-    1-Aug 16:00 PDT when the
-    <a href="https://www.bsideslv.org/talks#1045530">
-    "QuadBlockQuiz - Supply Chain Sandbox Edition"
-    </a>
-    talk starts.
-    The "1-hour" contest will be less than one hour and will be stopped
-    at a random time announced
-    by the speaker during the Q&A session for
-    <a href="https://www.bsideslv.org/talks#1045530">
-    "QuadBlockQuiz - Supply Chain Sandbox Edition"
-    </a>
-    The "24-hour" contest will run until 2-Aug 16:00 PDT.
-    </p>
-    <p>Time will be kept by game - don't start until AFTER the talk has started and the Contest Scoreboard activates.
+    The game must be played during eligle times.
+    An eligible games must start and end during times announced during the workshop.
+    The times will be on breaks, during the Q&A,
+    and for 3 hours after the conclusion of the workshop.
+    Games spanning timeslots will be ineligible
+    (eg if start during first break and finish during second break).
     Make sure to finish your game cleanly so that you have seen the endgame screen with your score.
     </p>
     <p>
-    Contestants may submit more than one game (the contest scoreboard will automatically do this for you), but are only eligible for one prize.
+    Contestants may submit more than one game (the contest scoreboard will automatically do this for you),
+    but only one prize per person.
+    </p>
+    <p>
+    You must enter the contest to win!!!!
+    When you start to play each game, it will ask whether you want to be part of this contest.
+    You must enter the contest to be on the Contest Scoreboard.
     </p>
     <p>
     Winners will be chosen based on their positions on the Contest Scoreboards.
     Note the Contest Scoreboard is different than the Leaderboard (which is for "all-time").
-    The Contest Scoreboard only shows games that started after the  contest start time and finished prior to the contest endtime.
     </p>
     <p>
-    For the "1-hour" Contest, contestants must participate in the awards ceremony during Q&A for
-    <a href="https://www.bsideslv.org/talks#1045530">
-    "QuadBlockQuiz - Supply Chain Sandbox Edition"
-    </a>
-    to win. Winners for the "24-hour" Contest will be contacted based on contact information
-    associated with their logon credentials.
-    They want to contact the contest organizer directly to make sure.
+    The "most points" prize will be awarded to the eligible
+    contest contestant with the highest points,
+    who will then be ineligible for the other Contest prizes.
     </p>
     <p>
-    Prizes are described at
-    <a href= <%= Routes.live_path(QuadquizaminosWeb.Endpoint, QuadquizaminosWeb.ContestPrizes) %> >prizes</a>.
+    The "most bricks" prize will be awarded to the eligible
+    contest contestant with the most number of bricks in one game,
+    who will then be ineligible for the other Contest prizes.
     </p>
     <p>
-    The "Custom Cocktail Instruction" prize will be awarded to the eligible
-    "1-hour" contest contestant with the highest points,
-    who will then be ineligible for more "1-hour" Contest prizes,
-    and also ineligible for the "24-hour" Contest "Absinthe Tutorial" prize.
+    The "most questions" prize will be awarded to the eligible
+    contest contestant who answered correctly the most trivia questions,
+    who will then be ineligible for the other Contest prizes.
     </p>
     <p>
-    The "Risk Book & Custom Cocktail Recipe" prize will be awarded to the eligible
-    "1-hour" contest contestant with the most answered questions,
-    who will then be ineligible either of the next prize.
-    </p>
-    <p>
-    The "Tee-shirt & Custom Cocktail Recipe" prize will be awarded to the eligible
-    "1-hour" contest contestant with the most bricks.
-    </p>
-
-    <p>
-    The "Absinthe Tutorial" prize will be awarded to the eligible
-    "24-hour" contest contestant with the highest points,
-    who will then be ineligible for more "24-hour" Contest prizes.
-    </p>
-    <p>
-    The "Risk Book & Custom Cocktail Recipe" prize will be awarded to the eligible
-    "24-hour" contest contestant with the most answered questions,
-    who will then be ineligible for more "24-hour" Contest prizes.
-    </p>
-    <p>
-    The "Custom Cocktail Recipe" prize will be awarded to the eligible
-    "24-hour" contest contestant with the most bricks.
+    The "????" prize will be awarded to the eligible
+    contest contestant based on surpise criteria announced
+    after the conclusion of the contest.
     </p>
     <p>
     A person is only elegible to receive one prize per contest,
-    (e.g first on points and first on bricks would only receive one prize)
-    or with multiple ID's.
-    </p>
-    <p>
-    The first person to pick a prize will be the eligible contestant with the highest point score.
-    The second person to pick a prize will be the next eligible contestant with the most dropped bricks.
-    The third person to pick a prize will be the next eligible contestant with the most answered questions.
-    The fourth person to pick a prize will be the next eligible contestant with the next highest point score.
-    Selection will continue until we run out of prizes or people.
-    </p>
-    <p>
-    Previous winners of the "Custom Cocktail Instruction" prize at RSAC (ie Oddie)
-    are not elibible to win that prize again, but are eligible for all other prizes.
-    Previous winners of the "Absinthe Tutorial" prize at RSAC (ie Rick)
-    are not elibible to win that prize again, but are eligible for all other prizes.
-    All other RSAC prize winners are eligible for all prizes at BSidesLV.
-    The winner of the BSidesLV "Custom Cocktail Instruction" prize
-    in the "1-hour" contest
-    is not eligible for the "Absinthe Tutorial" prize in the "24-hour" contest.
+    (e.g first on points and first on bricks would only receive one prize
+    for points, and then 'runner up' on bricks would win that prize).
     </p>
     <p>
     The organizers retain the right to adjust or shutdown the contest
@@ -144,10 +106,12 @@ defmodule QuadquizaminosWeb.ContestRules do
     <a href= <%= Routes.live_path(QuadquizaminosWeb.Endpoint, QuadquizaminosWeb.PrivacyLive) %> > Privacy </a>
     policy and our <a href= <%= Routes.live_path(QuadquizaminosWeb.Endpoint, QuadquizaminosWeb.TermsOfServiceLive) %> > Terms Of Service </a>.
     The TL;DR version is
-    we only use your ID to put your name (as provided publicly by your ID) on the
-    Leaderboard and Contest Scoreboard, and your email (as provided by your ID)
+    we only use your handle on the
+    Leaderboard and Contest Scoreboard, and your email
     for the purpose of contacting you regarding prizes.
     </p>
+
+
     """
   end
 end
