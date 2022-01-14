52d4c61bca2221c672bdfd5cc0b89901ed8a95bc
TraceyOnim <traceyonim22@gmail.com>
2022-01-14 15:21:00 +0300

    fix warning due deprecation

--- lib/quadquizaminos_web/live/live_helpers.ex
+++ lib/quadquizaminos_web/live/live_helpers.ex
@@ -19,6 +19,6 @@ defmodule QuadquizaminosWeb.LiveHelpers do
   def live_modal(socket, component, opts) do
     path = Keyword.fetch!(opts, :return_to)
     modal_opts = [id: :modal, return_to: path, component: component, opts: opts]
-    live_component(socket, QuadquizaminosWeb.ModalComponent, modal_opts)
+    live_component(QuadquizaminosWeb.ModalComponent, modal_opts)
   end
 end
