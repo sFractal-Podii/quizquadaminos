766123e9357d0f38c6bb42de4f847c5944f495bc
japhetb <japhetbrando@gmail.com>
2022-03-11 12:40:07 +0300

    Fix entry piont

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ./prod/rel/quadblockquiz/bin/quadblockquiz eval Quadblockquiz.ReleaseTask.createdb
-./prod/rel/quadblockquiz/bin/quadBlockquiz eval Quadblockquiz.ReleaseTask.migrate
+./prod/rel/quadblockquiz/bin/quadblockquiz eval Quadblockquiz.ReleaseTask.migrate
 ./prod/rel/quadblockquiz/bin/quadblockquiz start
