7c44781ffa490032ca9d0ec0d7ba564a19080009
japhetb <japhetbrando@gmail.com>
2022-05-04 09:29:30 +0300

    Testing the footer content

--- test/quadblockquiz_web/views/layout_view_test.exs
+++ test/quadblockquiz_web/views/layout_view_test.exs
@@ -5,4 +5,14 @@ defmodule QuadblockquizWeb.LayoutViewTest do
   # use functions such as safe_to_string() to convert the helper
   # result into an HTML string.
   # import Phoenix.HTML
+
+  test "user can see a table of footer content", %{conn: conn} do
+    conn = get(conn, "/")
+    assert conn.resp_body =~ "<a href= /contests >Contests</a>"
+    assert conn.resp_body =~ "<a href= /contest-prizes >Contest prizes</a>"
+    assert conn.resp_body =~ "<a href= /contest_rules >Contest rules</a>"
+    assert conn.resp_body =~ "<a href= /termsofservice > Terms Of Service </a>"
+    assert conn.resp_body =~ "<a href= /privacy > Privacy </a>"
+    assert conn.resp_body =~ "<a href= /.well-known/sbom >SBOM</a>"
+  end
 end
