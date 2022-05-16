0575d63396cd8e76c07342c35dca3ad8041dd8bc
sparrell <duncan@sfractal.com>
2022-05-15 21:50:39 -0400

    add tests, make scoring so bonus multiplier obvious (constant at front)

--- lib/quadblockquiz/scoring.ex
+++ lib/quadblockquiz/scoring.ex
@@ -10,6 +10,15 @@ defmodule Quadblockquiz.Scoring do
     %{name: "lethargic", bonus: 0}
   ]
 
+  @answer_multiplier [
+    %{name: "none", multiplier: 1},
+    %{name: "1 to 9", multiplier: 2},
+    %{name: "10 to 19", multiplier: 3},
+    %{name: "20 to 49", multiplier: 5},
+    %{name: "50 to 99", multiplier: 7},
+    %{name: "100 or more", multiplier: 11}
+  ]
+
   def tick(speed) do
     Enum.at(@drop_speeds, speed).bonus
   end
@@ -28,37 +37,37 @@ defmodule Quadblockquiz.Scoring do
 
   defp row_question_bonus(base, 0) do
     # no answers, no bonus
-    base
+    base * Enum.at(@answer_multiplier, 0).multiplier
   end
 
   defp row_question_bonus(base, correct_answers)
     when correct_answers in 1..9 do
       # answered a few so double
-      base * 2
+      base * Enum.at(@answer_multiplier, 1).multiplier
   end
 
   defp row_question_bonus(base, correct_answers)
     when correct_answers in 10..19 do
       # triple for 10-19 answers
-      base * 3
+      base * Enum.at(@answer_multiplier, 2).multiplier
   end
 
   defp row_question_bonus(base, correct_answers)
     when correct_answers in 20..49 do
       # 5x for 20-49 answers
-      base * 5
+      base * Enum.at(@answer_multiplier, 3).multiplier
   end
 
   defp row_question_bonus(base, correct_answers)
     when correct_answers in 50..99 do
       # 5x for 20-49 answers
-      base * 7
+      base * Enum.at(@answer_multiplier, 4).multiplier
   end
 
   defp row_question_bonus(base, correct_answers)
     when correct_answers >= 100 do
       # 5x for 20-49 answers
-      base * 11
+      base * Enum.at(@answer_multiplier, 5).multiplier
   end
 
 end
