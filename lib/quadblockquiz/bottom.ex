d238f2eb45315f0b9037f83494e5bcbcdcd4ae23
sparrell <duncan@sfractal.com>
2022-05-24 07:10:18 -0400

    format

--- lib/quadblockquiz/bottom.ex
+++ lib/quadblockquiz/bottom.ex
@@ -96,6 +96,7 @@ defmodule Quadblockquiz.Bottom do
   def add_vulnerability(bottom) do
     ## don't put on top of existing issue
     clean = remove_trouble_blocks(bottom)
+
     if clean != %{} do
       # some clean blocks in bottom, pick random one and add issue
       {{x, y}, _} = Enum.random(clean)
@@ -110,6 +111,7 @@ defmodule Quadblockquiz.Bottom do
   def add_license_issue(bottom) do
     ## don't put on top of existing issue
     clean = remove_trouble_blocks(bottom)
+    
     if clean != %{} do
       # some clean blocks in bottom, pick random one and add issue
       {{x, y}, _} = Enum.random(clean)
