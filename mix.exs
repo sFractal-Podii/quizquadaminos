54d9bf08eeb17b15d71f1c2d85cf7c90b39d7cd0
sparrell <duncan@sfractal.com>
2022-06-05 22:54:26 -0400

    troubleshooting start qna

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Quadblockquiz.MixProject do
   def project do
     [
       app: :quadblockquiz,
-      version: "0.19.1",
+      version: "0.19.2",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
