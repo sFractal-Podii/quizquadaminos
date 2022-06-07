38df26848aa865c237777bbc25d719df892d8a0d
sparrell <duncan@sfractal.com>
2022-06-07 16:39:47 -0400

    inc ver

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Quadblockquiz.MixProject do
   def project do
     [
       app: :quadblockquiz,
-      version: "0.21.1",
+      version: "0.22.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
