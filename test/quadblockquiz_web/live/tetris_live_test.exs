a01fcaf8559012abeede1ce7fd4bba5c3be17020
TraceyOnim <traceyonim22@gmail.com>
2023-09-18 10:48:45 +0300

    revert dependencies

--- test/quadblockquiz_web/live/tetris_live_test.exs
+++ test/quadblockquiz_web/live/tetris_live_test.exs
@@ -110,10 +110,6 @@ defmodule QuadblockquizWeb.TetrisLiveTest do
 
       score_to_display = score["Right"]
 
-      IO.inspect("<h1>" <> score_to_display <> "</h1>",
-        label: "============================whats happening"
-      )
-
       assert html =~ "<h1>" <> score_to_display <> "</h1>"
 
       render_keydown(view, "keydown", %{"key" => " "})
