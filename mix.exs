e3b7278912ceab1942639402f4407535a9001ee0
TraceyOnim <traceyonim22@gmail.com>
2023-09-18 10:52:29 +0300

    revert dependencies

--- mix.exs
+++ mix.exs
@@ -55,18 +55,18 @@ defmodule Quadblockquiz.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.7.7", override: true},
+      {:phoenix, "~> 1.6.16"},
       {:phoenix_ecto, "~> 4.4.2"},
       {:ecto_sql, "~> 3.10"},
       {:postgrex, ">= 0.17.2"},
-      {:phoenix_live_view, "~> 0.19.5", override: true},
+      {:phoenix_live_view, "~> 0.17.14"},
       {:floki, ">= 0.34.3", only: :test},
       {:phoenix_html, "~> 3.3.1"},
       {:phoenix_live_reload, "~> 1.4.1", only: :dev},
-      {:phoenix_live_dashboard, "~>  0.8.1"},
+      {:phoenix_live_dashboard, "~> 0.6"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
-      {:gettext, "~> 0.23.1", override: true},
+      {:gettext, "~> 0.22.3"},
       {:jason, "~> 1.4.1"},
       {:plug_cowboy, "~> 2.6.1"},
       {:oauth2, "~> 2.1.0", override: true},
