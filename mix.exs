29ea47a14d8c3f0f6f48696813b1302ec62cf599
Duncan Sparrell <sparrell@users.noreply.github.com>
2023-12-15 08:31:56 -0500

    Update mix.exs
    inc ver to kick alpha

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Quadblockquiz.MixProject do
     [
       app: :quadblockquiz,
       description: "Descri'be",
-      version: "0.28.5",
+      version: "0.28.6",
       elixir: "~> 1.15.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix] ++ Mix.compilers(),
