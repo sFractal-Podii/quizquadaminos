448ae871ac6c4a0504e256d2bbad3429b4ae9397
TraceyOnim <traceyonim22@gmail.com>
2024-02-05 10:11:21 +0300

    fix negative scores

--- lib/quadblockquiz/qna.ex
+++ lib/quadblockquiz/qna.ex
@@ -108,9 +108,9 @@ defmodule Quadblockquiz.QnA do
 
         score
         |> String.trim()
-        |> String.split("-", trim: true)
+        |> String.split("- ", trim: true)
         |> Enum.map(fn score ->
-          score |> String.trim() |> String.split(":") |> List.to_tuple() |> modify_score()
+          score |> String.trim() |> String.split(":") |> List.to_tuple()
         end)
         |> Map.new()
 
@@ -118,32 +118,6 @@ defmodule Quadblockquiz.QnA do
         %{"Right" => "25", "Wrong" => "5"}
     end
   end
-
-  defp modify_score({"Right", score}) do
-    regex = ~r/\d+/
-
-    if Regex.match?(~r/negative/, score) do
-      [score | _] = Regex.run(regex, score)
-
-      {"Right", "-" <> score}
-    else
-      {"Right", score}
-    end
-  end
-
-  defp modify_score({"Wrong", score}) do
-    regex = ~r/\d+/
-
-    if Regex.match?(~r/negative/, score) do
-      [score | _] = Regex.run(regex, score)
-
-      {"Wrong", "-" <> score}
-    else
-      {"Wrong", score}
-    end
-  end
-
-  defp modify_score(score), do: score
 
   defp powerup(content) do
     regex = ~r/# powerup(?<powerup>.*)/i
