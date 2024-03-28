efba81af4dd0b079d0ae91f15fa8dac5e59384ed
Duncan Sparrell <duncan@sfractal.com>
2024-03-28 08:14:27 -0400

    logos, formating, inc version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Quadblockquiz.MixProject do
     [
       app: :quadblockquiz,
       description: "Descri'be",
-      version: "0.28.9",
+      version: "0.28.10",
       elixir: "~> 1.15.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix] ++ Mix.compilers(),
