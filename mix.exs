0516141fa49bbc7a9fc04ba05a8d9a300e526fec
TraceyOnim <traceyonim22@gmail.com>
2024-04-05 08:31:23 +0300

    update version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Quadblockquiz.MixProject do
     [
       app: :quadblockquiz,
       description: "Descri'be",
-      version: "1.0.3",
+      version: "1.0.4",
       elixir: "~> 1.15.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
