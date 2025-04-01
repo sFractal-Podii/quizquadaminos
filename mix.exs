a405351ba06f6773afa943f88e03b8651c98423f
Sigu Magwa <sigu@users.noreply.github.com>
2025-04-01 13:45:55 +0300

    Update mix.exs
    alpha 1.1.14

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Quadblockquiz.MixProject do
     [
       app: :quadblockquiz,
       description: "Descri'be",
-      version: "1.1.14-dev",
+      version: "1.1.14-alpha",
       elixir: "~> 1.15.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
