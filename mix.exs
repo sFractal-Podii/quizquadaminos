fafb9a0b21e0407d970cde198c3ac035066f467a
Sigu Magwa <sigumagwa@gmail.com>
2025-07-01 18:12:43 +0300

    upgrade erlang

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Quadblockquiz.MixProject do
     [
       app: :quadblockquiz,
       description: "Describe",
-      version: "1.1.14",
+      version: "1.1.15-dev",
       elixir: "~> 1.15.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
