c194e352f49117bceb54884ced64ab2bd3655114
Sigu Magwa <sigumagwa@gmail.com>
2025-07-01 19:08:59 +0300

    prep for dev

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Quadblockquiz.MixProject do
     [
       app: :quadblockquiz,
       description: "Describe",
-      version: "1.1.15",
+      version: "1.1.16-dev",
       elixir: "~> 1.15.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
