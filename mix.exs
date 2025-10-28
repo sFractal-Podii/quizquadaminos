69a8bbbf112861adcc7afb25fa3da03ac5036316
Sigu Magwa <sigumagwa@gmail.com>
2025-10-28 15:43:08 +0300

    bump version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Quadblockquiz.MixProject do
     [
       app: :quadblockquiz,
       description: "Describe",
-      version: "1.1.16-dev",
+      version: "1.1.16-alpha",
       elixir: "~> 1.15.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
